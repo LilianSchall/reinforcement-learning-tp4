70516d58324b41c8db9783fc00e13739f026d5b3
Lilian Schall <lilian.schall@epita.fr>
2024-11-09 11:29:18 +0100

    feat: added basic requirements

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+(pkgs.mkShell {
+  name = "pip-env";
+  nativeBuildInputs = with pkgs; [
+    python3
+    python3Packages.pip
+    python3Packages.virtualenv
+    stdenv.cc
+    gnumake
+    black
+  ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+        pkgs.stdenv.cc.cc
+    ]};
+  '';
+})
