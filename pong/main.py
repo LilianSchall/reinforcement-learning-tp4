b89f841a301c63c8012c919fb8bf4cfcd8a17bc5
Lilian Schall <lilian.schall@epita.fr>
2024-11-17 14:14:57 +0100

    fix: fixed batch_size and moved video out of pong folder

--- pong/main.py
+++ pong/main.py
@@ -6,7 +6,7 @@ def main():
         nb_epochs=1500,
         max_nb_steps=10000, 
         memory_size=10000,
-        batch_size=64,
+        batch_size=32,
         training=True,
         checkpoint_frequency=100,
     )
