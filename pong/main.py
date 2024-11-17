0023798e344d8021d70da54dc8b515579c311726
Lilian Schall <lilian.schall@epita.fr>
2024-11-17 13:15:37 +0100

    fix: fixed parameters of session

--- pong/main.py
+++ pong/main.py
@@ -3,12 +3,12 @@ from pong.models import Session
 
 def main():
     session = Session(
-        nb_epochs=3000,
+        nb_epochs=1500,
         max_nb_steps=10000, 
-        memory_size=100000,
+        memory_size=10000,
         batch_size=64,
         training=True,
-        checkpoint_frequency=2,
+        checkpoint_frequency=100,
     )
 
     session.run()
